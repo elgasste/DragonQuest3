a8c1f188d7e4680d732197e1ee73d2e97600449d
Steve Elgas <elgasste@gmail.com>
2026-08-03 18:09:50 -0400

    Added clock tests to test_game

--- test/test_game.c
+++ test/test_game.c
@@ -24,6 +24,9 @@ typedef struct PixelBufferClearColorCall_t
 }
 PixelBufferClearColorCall_t;
 
+local_persist int g_clockInitCallCount;
+local_persist int g_clockStartFrameCallCount;
+local_persist int g_clockEndFrameCallCount;
 local_persist PixelBufferCreateCall_t g_pixelBufferCreateCall;
 local_persist int g_pixelBufferCreateCallCount;
 local_persist PixelBufferClearColorCall_t g_pixelBufferClearColorCall;
@@ -34,6 +37,10 @@ local_persist MemArena_t* g_memArena;
 
 void setUp( void )
 {
+   g_clockInitCallCount = 0;
+   g_clockStartFrameCallCount = 0;
+   g_clockEndFrameCallCount = 0;
+
    g_pixelBufferCreateCallCount = 0;
    g_pixelBufferCreateCall.memArena = 0;
    g_pixelBufferCreateCall.width = 0;
@@ -59,21 +66,24 @@ void tearDown( void )
 
 void Clock_Init( Clock_t* clock, u32 fps )
 {
-   // TODO
    UNUSED_PARAM( clock );
    UNUSED_PARAM( fps );
+
+   g_clockInitCallCount++;
 }
 
 void Clock_StartFrame( Clock_t* clock )
 {
-   // TODO
    UNUSED_PARAM( clock );
+
+   g_clockStartFrameCallCount++;
 }
 
 void Clock_EndFrame( Clock_t* clock )
 {
-   // TODO
    UNUSED_PARAM( clock );
+
+   g_clockEndFrameCallCount++;
 }
 
 void PixelBuffer_Create( PixelBuffer_t** pBuffer, MemArena_t* memArena, u32 w, u32 h )
@@ -129,8 +139,8 @@ void test_Game_Create_CreatesGameWithCorrectParameters( void )
    Game_t game;
 
    Game_Create( &game, g_memArena );
-
    TEST_ASSERT_EQUAL( g_memArena, game.memArena );
+   TEST_ASSERT_EQUAL( 1, g_clockInitCallCount );
    TEST_ASSERT_EQUAL( 1, g_pixelBufferCreateCallCount );
    TEST_ASSERT_EQUAL( g_memArena, g_pixelBufferCreateCall.memArena );
    TEST_ASSERT_EQUAL( SCREEN_WIDTH, g_pixelBufferCreateCall.width );
@@ -148,7 +158,8 @@ void test_Game_Run_StopsAfterMultipleMessageHandlerTicks( void )
    Game_Create( &game, g_memArena );
 
    Game_Run( &game );
-
+   TEST_ASSERT_EQUAL( 3, g_clockStartFrameCallCount );
+   TEST_ASSERT_EQUAL( 3, g_clockEndFrameCallCount );
    TEST_ASSERT_EQUAL( 3, g_platformHandleMessagesCallCount );
    TEST_ASSERT_EQUAL( 3, g_platformRenderScreenBufferCallCount );
    TEST_ASSERT_EQUAL( 3, g_pixelBufferClearColorCall.callCount );
@@ -167,7 +178,6 @@ void test_Game_Run_StopsAfterMultipleRenderHandlerTicks( void )
    Game_Create( &game, g_memArena );
 
    Game_Run( &game );
-
    TEST_ASSERT_EQUAL( 3, g_platformHandleMessagesCallCount );
    TEST_ASSERT_EQUAL( 3, g_platformRenderScreenBufferCallCount );
    TEST_ASSERT_EQUAL( 3, g_pixelBufferClearColorCall.callCount );
