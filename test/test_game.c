0d15864adfe3f73c656f0447ce86f811a4faf38d
Steve Elgas <elgasste@gmail.com>
2026-08-02 21:30:27 -0400

    Added tests for game render and shutdown

--- test/test_game.c
+++ test/test_game.c
@@ -14,8 +14,20 @@ typedef struct PixelBufferCreateCall_t
 }
 PixelBufferCreateCall_t;
 
+typedef struct PixelBufferClearColorCall_t
+{
+   PixelBuffer_t* buffer;
+   u32 color;
+   int callCount;
+}
+PixelBufferClearColorCall_t;
+
 local_persist PixelBufferCreateCall_t g_pixelBufferCreateCall;
 local_persist int g_pixelBufferCreateCallCount;
+local_persist PixelBufferClearColorCall_t g_pixelBufferClearColorCall;
+local_persist int g_messageHandlerCallCount;
+local_persist int g_renderHandlerCallCount;
+local_persist Game_t* g_currentGame;
 
 void setUp( void )
 {
@@ -24,6 +36,14 @@ void setUp( void )
    g_pixelBufferCreateCall.width = 0;
    g_pixelBufferCreateCall.height = 0;
    g_pixelBufferCreateCall.createdBuffer = 0;
+
+   g_pixelBufferClearColorCall.buffer = 0;
+   g_pixelBufferClearColorCall.color = 0;
+   g_pixelBufferClearColorCall.callCount = 0;
+
+   g_messageHandlerCallCount = 0;
+   g_renderHandlerCallCount = 0;
+   g_currentGame = 0;
 }
 
 void tearDown( void ) {}
@@ -47,8 +67,27 @@ void PixelBuffer_Create( PixelBuffer_t** pBuffer, MemArena_t* memArena, u32 w, u
 
 void PixelBuffer_ClearColor( PixelBuffer_t* buffer, u32 color )
 {
-   UNUSED_PARAM( buffer );
-   UNUSED_PARAM( color );
+   g_pixelBufferClearColorCall.buffer = buffer;
+   g_pixelBufferClearColorCall.color = color;
+   g_pixelBufferClearColorCall.callCount++;
+}
+
+void TestMessageHandler( void )
+{
+   g_messageHandlerCallCount++;
+   if ( g_currentGame != 0 && g_messageHandlerCallCount >= 3 )
+   {
+      Game_Stop( g_currentGame );
+   }
+}
+
+void TestRenderHandler( void )
+{
+   g_renderHandlerCallCount++;
+   if ( g_currentGame != 0 && g_renderHandlerCallCount >= 3 )
+   {
+      Game_Stop( g_currentGame );
+   }
 }
 
 void test_Game_Create_CreatesGameWithCorrectParameters( void )
@@ -75,11 +114,55 @@ void test_Game_Create_CreatesGameWithCorrectParameters( void )
    TEST_ASSERT_EQUAL( render, game.platformRender );
 }
 
+void test_Game_Run_StopsAfterMultipleMessageHandlerTicks( void )
+{
+   MemArena_t arena;
+   Game_t game;
+
+   memset( &arena, 0, sizeof( arena ) );
+   memset( &game, 0, sizeof( game ) );
+
+   g_currentGame = &game;
+   Game_Create( &game, &arena, TestMessageHandler, TestRenderHandler );
+
+   Game_Run( &game );
+
+   TEST_ASSERT_EQUAL( 3, g_messageHandlerCallCount );
+   TEST_ASSERT_EQUAL( 3, g_renderHandlerCallCount );
+   TEST_ASSERT_EQUAL( 3, g_pixelBufferClearColorCall.callCount );
+   TEST_ASSERT_EQUAL( game.pixelBuffer, g_pixelBufferClearColorCall.buffer );
+   TEST_ASSERT_EQUAL( 0, g_pixelBufferClearColorCall.color );
+   TEST_ASSERT_TRUE( game.shutdown );
+}
+
+void test_Game_Run_StopsAfterMultipleRenderHandlerTicks( void )
+{
+   MemArena_t arena;
+   Game_t game;
+
+   memset( &arena, 0, sizeof( arena ) );
+   memset( &game, 0, sizeof( game ) );
+
+   g_currentGame = &game;
+   Game_Create( &game, &arena, TestMessageHandler, TestRenderHandler );
+
+   Game_Run( &game );
+
+   TEST_ASSERT_EQUAL( 3, g_messageHandlerCallCount );
+   TEST_ASSERT_EQUAL( 3, g_renderHandlerCallCount );
+   TEST_ASSERT_EQUAL( 3, g_pixelBufferClearColorCall.callCount );
+   TEST_ASSERT_EQUAL( game.pixelBuffer, g_pixelBufferClearColorCall.buffer );
+   TEST_ASSERT_EQUAL( 0, g_pixelBufferClearColorCall.color );
+   TEST_ASSERT_TRUE( game.shutdown );
+}
+
 int main( void )
 {
    UNITY_BEGIN();
 
    RUN_TEST( test_Game_Create_CreatesGameWithCorrectParameters );
+   RUN_TEST( test_Game_Run_StopsAfterMultipleMessageHandlerTicks );
+   RUN_TEST( test_Game_Run_StopsAfterMultipleRenderHandlerTicks );
 
    return UNITY_END();
 }
