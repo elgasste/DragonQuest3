a1f4f8e82a4b19beb3941bd7517f259f1aebc345
Steve Elgas <elgasste@gmail.com>
2026-08-04 16:27:29 -0400

    Copied input module from DW3Arduino and added tests

---
+++ include/input.h
@@ -0,0 +1,41 @@
+#if !defined ( INPUT_H )
+#define INPUT_H
+
+#include "common.h"
+
+typedef enum InputButton_t
+{
+   InputButton_A = 0,
+   InputButton_B,
+   InputButton_Select,
+   InputButton_Start,
+   InputButton_Up,
+   InputButton_Down,
+   InputButton_Left,
+   InputButton_Right,
+
+   InputButton_Count
+}
+InputButton_t;
+
+typedef struct InputButtonState_t
+{
+   b32 pressed;
+   b32 released;
+   b32 down;
+}
+InputButtonState_t;
+
+typedef struct Input_t
+{
+   InputButtonState_t buttonStates[InputButton_Count];
+}
+Input_t;
+
+void Input_Init( Input_t* input );
+void Input_ResetPressStates( Input_t* input );
+void Input_PressButton( Input_t* input, InputButton_t button );
+void Input_ReleaseButton( Input_t* input, InputButton_t button );
+b32 Input_AnyButtonPressed( Input_t* input );
+
+#endif // INPUT_H
