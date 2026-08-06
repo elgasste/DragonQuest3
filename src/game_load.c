d481dab503a82cca9e893a645565d6e8f15d9f19
selgas <s.elgas@techsmith.com>
2026-08-06 17:25:30 -0400

    Load the game data file on startup and verify header

--- src/game_load.c
+++ src/game_load.c
@@ -1,6 +1,10 @@
+#include <string.h>
+
 #include "game.h"
 #include "mem_arena.h"
 #include "platform_ops.h"
+
+internal void GameData_VerifyHeaderAndVersion( u8* fileContents, u32 fileSize );
 
 void Game_LoadFromFile( Game_t* game, const char* filePath )
 {
@@ -9,7 +13,32 @@ void Game_LoadFromFile( Game_t* game, const char* filePath )
 
    fileContents = PlatformOps_LoadFileToMemory( filePath, game->memArena, &fileSize );
 
-   // TODO: load stuff!
+   GameData_VerifyHeaderAndVersion( fileContents, fileSize );
 
    MemArena_Free( game->memArena, fileContents );
+}
+
+internal void GameData_VerifyHeaderAndVersion( u8* fileContents, u32 fileSize )
+{
+   GameDataHeader_t* header;
+
+   if ( fileSize < sizeof( GameDataHeader_t ) )
+   {
+      PlatformOps_FatalError( "Game data file is too small to contain a valid header." );
+      return;
+   }
+
+   header = (GameDataHeader_t*)fileContents;
+
+   if ( strncmp( header->magic, GAME_DATA_MAGIC, 4 ) != 0 )
+   {
+      PlatformOps_FatalError( "Game data file has an invalid magic number." );
+      return;
+   }
+   else if ( header->version.major != GAME_DATA_VERSION_MAJOR ||
+             header->version.minor != GAME_DATA_VERSION_MINOR ||
+             header->version.maint != GAME_DATA_VERSION_MAINT )
+   {
+      PlatformOps_FatalError( "Game data file has an incompatible version." );
+   }
 }
