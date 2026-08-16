f0aad4f9020994e566e1797a34f5fe950b63115f
Steve Elgas <elgasste@gmail.com>
2026-08-15 21:41:33 -0400

    Teleport the player on wrapping edges

--- src/game_input.c
+++ src/game_input.c
@@ -25,18 +25,46 @@ void Game_HandleInput( Game_t* game )
 
    if ( game->playerRect.x < 0 )
    {
-      game->playerRect.x = 0;
+      if ( game->tileMap->wraps )
+      {
+         game->playerRect.x = ( game->tileMap->tilesX * game->tileTextureSet->tileSize ) - game->playerRect.w;
+      }
+      else
+      {
+         game->playerRect.x = 0;
+      }
    }
    if ( game->playerRect.y < 0 )
    {
-      game->playerRect.y = 0;
+      if ( game->tileMap->wraps )
+      {
+         game->playerRect.y = ( game->tileMap->tilesY * game->tileTextureSet->tileSize ) - game->playerRect.h;
+      }
+      else
+      {
+         game->playerRect.y = 0;
+      }
    }
    if ( game->playerRect.x + game->playerRect.w > (i32)( game->tileMap->tilesX * game->tileTextureSet->tileSize ) )
    {
-      game->playerRect.x = ( game->tileMap->tilesX * game->tileTextureSet->tileSize ) - game->playerRect.w;
+      if ( game->tileMap->wraps )
+      {
+         game->playerRect.x = 0;
+      }
+      else
+      {
+         game->playerRect.x = ( game->tileMap->tilesX * game->tileTextureSet->tileSize ) - game->playerRect.w;
+      }
    }
    if ( game->playerRect.y + game->playerRect.h > (i32)( game->tileMap->tilesY * game->tileTextureSet->tileSize ) )
    {
-      game->playerRect.y = ( game->tileMap->tilesY * game->tileTextureSet->tileSize ) - game->playerRect.h;
+      if ( game->tileMap->wraps )
+      {
+         game->playerRect.y = 0;
+      }
+      else
+      {
+         game->playerRect.y = ( game->tileMap->tilesY * game->tileTextureSet->tileSize ) - game->playerRect.h;
+      }
    }
 }
