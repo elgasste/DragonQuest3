04adba84778c721fece117fdc7d8044de18ae0a0
Steve Elgas <elgasste@gmail.com>
2026-08-22 19:37:30 -0400

    not sure where this came from

--- include/win_common.h
+++ include/win_common.h
@@ -55,4 +55,4 @@ extern WinCornerPopup_t g_winCornerPopup;
 // win_test_game_data.c
 void WriteTestGameDataFile( const char *filePath );
 
-#endif // !defined( WIN_COMMON_H )
+#endif // WIN_COMMON_H
